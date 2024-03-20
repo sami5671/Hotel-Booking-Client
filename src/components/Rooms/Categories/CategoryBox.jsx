444fb97329b0197542a9386ec376e3b2d921ced5
sami5671 <samialam5671@gmail.com>
2024-03-20 09:30:03 +0600

    using query-string package to locate the browser uri query

--- src/components/Rooms/Categories/CategoryBox.jsx
+++ src/components/Rooms/Categories/CategoryBox.jsx
@@ -1,8 +1,32 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
+
 const CategoryBox = ({ label, icon: Icon }) => {
   //   console.log(category);
 
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // console.log("clicked", label);
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      const updateQuery = { ...currentQuery, category: label };
+
+      const url = qs.stringifyUrl({
+        url: "/",
+        query: updateQuery,
+      });
+      navigate(url);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer">
+    <div
+      onClick={handleClick}
+      className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer"
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
